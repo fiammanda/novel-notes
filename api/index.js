5f1e637dcc901a9942fd7a29045f9e2490f61b10
fiammanda <fiammanda@gmail.com>
2026-09-14 17:07:01 +0800

    add redis logging

--- api/index.js
+++ api/index.js
@@ -19,13 +19,51 @@ app.use("/*", async (c, next) => {
 app.get("/data.js", etag(), async (c) => {
   if (!c.get("same-site")) return c.notFound();
   const auth = c.get("auth");
-  const data = await redis.get();
+  const data = await redis.get("data");
   c.header("Vary", "Cookie");
   c.header("Content-Type", "application/javascript; charset=UTF-8");
   auth
     ? c.header("Cache-Control", "private, max-age=60")
     : c.header("Cache-Control", "public, max-age=60, s-maxage=86400, stale-while-revalidate=86400");
-  return c.body(`window.AUTH=${JSON.stringify(auth)};window.DATA=${JSON.stringify(data.sort((a, b) => b.update.localeCompare(a.update)))};`);
+  return c.body(`window.AUTH=${JSON.stringify(auth)};window.DATA=${JSON.stringify(Object.values(data).sort((a, b) => b.update.localeCompare(a.update)))};`);
+});
+
+app.get("/api/logs", async (c) => {
+  if (!c.get("auth")) return c.notFound();
+  const ua = c.req.header("user-agent") || "";
+  const hash = await redis.get("logs");
+  const logs = Object.entries(hash).reverse();
+
+  c.header("Vary", "Cookie");
+  c.header("Cache-Control", "public, max-age=60, s-maxage=36000, stale-while-revalidate=36000");
+
+  if (/iP(hone|od|ad)/.test(ua)) {
+    const html = `<!DOCTYPE html><html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, interactive-widget=resizes-content" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="stylesheet" href="/style.css" />
+        <style>
+          :root { color-scheme: light dark; }
+          body { font: .875rem/1 var(--font-mono); color: CanvasText; background: Canvas; }
+          table { width: 100%; border-spacing: 0; }
+          thead { position: sticky; top: 0; background: color-mix(in srgb, Canvas 20%, transparent); backdrop-filter: blur(.25rem); }
+          th { font-weight: 400; text-align: left; }
+          th, td { padding: .5em; border-bottom: 1px solid color-mix(in srgb, CanvasText 20%, transparent); }
+        </style>
+      </head>
+      <body>
+        <table>
+          <thead><tr><th>timestamp</th><th>message</th></tr></thead>
+          <tbody>${logs.map(([date, text]) => `<tr><td>${date}</td><td>${text}</td></tr>`).join("")}</tbody>
+        </table>
+      </body>
+    </html>`;
+    return c.html(html.replace(/\n\s+/g, ""));
+  }
+
+  return c.json(Object.fromEntries(logs));
 });
 
 app.get("/api/data/update", async (c) => {
@@ -33,10 +71,10 @@ app.get("/api/data/update", async (c) => {
   if (!auth) return c.notFound();
 
   const offset = Number(c.req.query("offset") || 0);
-  const number = [];
+  const hash = await redis.get("data");
+  let list = Object.values(hash);
+  let text = list.length;
 
-  let list = await redis.get();
-  number[0] = list.length;
   if (auth === "secret") {
     const date = new Date().getDate();
     const hour = new Date().getHours();
@@ -46,13 +84,17 @@ app.get("/api/data/update", async (c) => {
       status === "连载" && progress === "追读"
     );
   }
-  number[1] = list.length;
-  list = list.slice(offset, offset + 25);
-  if (!list.length) return c.json({ updated: `0/${number[1]} (${number[0]})` });
 
-  const bmap = new Map(list.map((item) => [item.url, item]));
+  text = list.length + " (" + text + ")"
+  list = list.slice(offset, offset + 25);
+
+  if (!list.length) {
+    await redis.log(`updated: 0/${text}`);
+    return c.json({ updated: `0/${text}` });
+  }
+
   const resp = await parse(list.map(({ url }) => url), true);
-  const data = resp.data.map(({ url, ...rest }) => ({ ...bmap.get(url), ...rest }));
+  const data = resp.data.map(({ id, ...rest }) => [id, { ...hash[id], ...rest }]);
   await redis.set(data);
 
   const next = offset + 25;
@@ -67,8 +109,8 @@ app.get("/api/data/update", async (c) => {
     });
   }
 
-  console.log(`updated: ${list.length}/${number[1]} (${number[0]})`);
-  return c.json({ updated: `${list.length}/${number[1]} (${number[0]})` });
+  await redis.log(`updated: ${list.length}/${text}`);
+  return c.json({ updated: `${list.length}/${text}` });
 });
 
 app.post("/api/data/meta", async (c) => {
@@ -80,7 +122,7 @@ app.post("/api/data/meta", async (c) => {
 app.post("/api/data", async (c) => {
   if (!c.get("auth")) return c.json({ error: "Unauthorized" }, 401);
   const list = await c.req.json();
-  const novel = list.map(({ upload, ...book }) => book);
+  const novel = list.map(({ upload, ...book }) => [book.id, book]);
   const cover = list.filter(({ upload }) => upload).map(({ id, url, upload }) => ({ id, url, upload }));
   const error = [ await redis.set(novel) ];
   let data = error[0].msg
